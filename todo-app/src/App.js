b067218a9792ba1de5669b431a71a2febca89414
Corey Kohler <kohler.nai@gmail.com>
2021-03-22 16:17:08 -0600

    app will save data to localstorage now

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -10,9 +10,12 @@ function App() {
   const [todos, setTodos] = useState([])
   const [status, setStatus] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
-
+  useEffect(()=>{
+    getLocalTodos()
+  },[])
   useEffect(()=>{
     filterHandler()
+    saveLocalTodos()
   }, [todos, status])
 
   const filterHandler = () => {
@@ -26,6 +29,18 @@ function App() {
       default:
         setFilteredTodos(todos);
         break;
+    }
+  }
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem("todos") === null){
+      localStorage.setItem("todos", JSON.stringify([]))
+    } else {
+      setTodos(JSON.parse(localStorage.getItem("todos")))
     }
   }
 
