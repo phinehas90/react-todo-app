95bc5a315a7f96e7c834506ca98e066cc493d317
Corey Kohler <kohler.nai@gmail.com>
2021-03-23 14:07:59 -0600

    modal added and opens onClick

--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Modal from 'react-modal';
+import {useState} from 'react';
 
 const Todo = (props) => {
     
     const deleteHandler = () => {
         props.setTodos(props.todos.filter(el => el.id !== props.todo.id))
     }
-
+    const [modalState, setModalState] = useState(false)
     const completeHandler = () => {
         props.setTodos(props.todos.map(item=>{
             if(item.id === props.todo.id){
@@ -16,6 +18,21 @@ const Todo = (props) => {
             return item
         }))
     }
+
+    const toggleModal = () => {
+        setModalState(!modalState)
+        console.log(props.todo.id)
+    }
+
+    const updateHandler = () => {
+        props.setTodos(props.todos.map(item => {
+            if(item.id === props.todo.id){
+                return {
+                    ...item, text: item.text
+                }
+            }
+        }))
+    }
     return (
         <div className="todo">
             <li className={props.todo.completed ? "todo-item completed": "todo-item"}>{props.todo.text}</li>
@@ -24,6 +41,9 @@ const Todo = (props) => {
             </button>
             <button onClick={deleteHandler} className="trash-btn">
                 <i className="fas fa-trash"></i>
+            </button>
+            <button onClick={toggleModal} className="complete-btn">
+                <Modal isOpen={modalState} className="complete-btn">Update</Modal>
             </button>
         </div>
     )
